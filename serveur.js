9065f0ae81afd3af6359eb6a2a3a76da79b56af4
RaouaRamdani <raouaram@gmail.com>
2026-08-11 09:51:38 +0100

    ajouter le port dynapique

--- serveur.js
+++ serveur.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const { jsPDF } = require('jspdf');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT ;
 
 app.use(cors());
 app.use(express.json());
